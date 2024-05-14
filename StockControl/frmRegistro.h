045eb455bbec2239c52cdd9296084ef4da2ab55b
MrGoose750 <elsotelito777@gmail.com>
2024-05-14 01:57:24 -0600

    se agregan formularios

---
+++ StockControl/frmRegistro.h
@@ -0,0 +1,157 @@
+#pragma once
+
+namespace StockControl {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de frmRegistro
+	/// </summary>
+	public ref class frmRegistro : public System::Windows::Forms::Form
+	{
+	public:
+		frmRegistro(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		~frmRegistro()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::TextBox^ textBox3;
+	protected:
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label1;
+
+	private:
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// textBox3
+			// 
+			this->textBox3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox3->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(1)), static_cast<System::Int32>(static_cast<System::Byte>(97)),
+				static_cast<System::Int32>(static_cast<System::Byte>(112)));
+			this->textBox3->Location = System::Drawing::Point(51, 331);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(381, 26);
+			this->textBox3->TabIndex = 11;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox2->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(1)), static_cast<System::Int32>(static_cast<System::Byte>(97)),
+				static_cast<System::Int32>(static_cast<System::Byte>(112)));
+			this->textBox2->Location = System::Drawing::Point(51, 222);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(381, 26);
+			this->textBox2->TabIndex = 10;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(1)), static_cast<System::Int32>(static_cast<System::Byte>(97)),
+				static_cast<System::Int32>(static_cast<System::Byte>(112)));
+			this->textBox1->Location = System::Drawing::Point(51, 120);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(381, 26);
+			this->textBox1->TabIndex = 9;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(1)), static_cast<System::Int32>(static_cast<System::Byte>(97)),
+				static_cast<System::Int32>(static_cast<System::Byte>(112)));
+			this->label3->Location = System::Drawing::Point(46, 270);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(262, 29);
+			this->label3->TabIndex = 8;
+			this->label3->Text = L"Confirmar contraseña";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(1)), static_cast<System::Int32>(static_cast<System::Byte>(97)),
+				static_cast<System::Int32>(static_cast<System::Byte>(112)));
+			this->label2->Location = System::Drawing::Point(46, 171);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(146, 29);
+			this->label2->TabIndex = 7;
+			this->label2->Text = L"Contraseña";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(1)), static_cast<System::Int32>(static_cast<System::Byte>(97)),
+				static_cast<System::Int32>(static_cast<System::Byte>(112)));
+			this->label1->Location = System::Drawing::Point(46, 65);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(241, 29);
+			this->label1->TabIndex = 6;
+			this->label1->Text = L"Nombre de Usuario";
+			// 
+			// frmRegistro
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(582, 425);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"frmRegistro";
+			this->Text = L"frmRegistro";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	};
+}
---
+++ StockControl/frmUsuario.cpp
@@ -0,0 +1,2 @@
+#include "frmUsuario.h"
+
