687b1d4f287b5c5390565246e5df8d08ff0c7b73
MrGoose750 <elsotelito777@gmail.com>
2024-05-17 15:08:38 -0600

    se agregan formularios

---
+++ StockControl/frmLogin.cpp
@@ -0,0 +1,11 @@
+#include "frmLogin.h"
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	StockControl::frmLogin form;
+	Application::Run(% form);
+
+
